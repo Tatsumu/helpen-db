cfed8cd4958f971ea459379ed87e594a3104b8aa
Tatsumu Matsui <t.m.555.tennis@gmail.com>
2016-03-15 16:04:33 +0000

    initial commit

---
+++ app/controllers/api/positions_controller.rb
@@ -0,0 +1,40 @@
+class Api::PositionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :create
+  def create
+    position = Position.new(create_params)
+    unless position.save
+        @error_message = [position.error.full_messages].compact
+    end
+  end
+  
+  def res
+  end
+  
+  def index
+  end
+  
+  def map
+    @points = Position.all.limit('1').order('created_at DESC')
+    @hash = Gmaps4rails.build_markers(@points) do |position, marker|
+      marker.lat position.lat
+      marker.lng position.long
+    end
+    y = Position.order('created_at DESC').pluck(:lat)[0]
+    x = Position.order('created_at DESC').pluck(:long)[0]
+    uri = URI("http://geoapi.heartrails.com/api/json")
+    uri.query = URI.encode_www_form({ method: "searchByGeoLocation", x: x, y: y })
+    res = Net::HTTP.get_response(uri)
+    json = JSON.parse(res.body)
+    # puts json
+    @city = json["response"]["location"][0]["city"]
+    @town = json["response"]["location"][0]["town"]
+      # marker.infowindow user.description
+      # marker.json({title: user.title})
+    
+  end
+
+  private
+  def create_params
+    params.permit(:lat, :long, :mail, :name)
+  end
+end
---
+++ db/migrate/20160315081541_create_positions.rb
@@ -0,0 +1,12 @@
+class CreatePositions < ActiveRecord::Migration
+  def change
+    create_table :positions do |t|
+      t.decimal  "lat",   precision: 16, scale: 13
+      t.decimal  "long",  precision: 16, scale: 13
+      t.string  "name", null: false
+      t.string  "mail", null: false
+
+      t.timestamps null: false
+    end
+  end
+end
